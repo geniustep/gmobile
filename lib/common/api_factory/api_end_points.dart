c56da3b6e882f8f9680fe90495d9bf3a726e5327
geniustep <agsteps@gmail.com>
2026-01-20 16:15:36 +0100

    good practis

--- lib/common/api_factory/api_end_points.dart
+++ lib/common/api_factory/api_end_points.dart
@@ -5,13 +5,13 @@ class ApiEndPoints {
   static const String destroy = "web/session/destroy";
   static const String authenticate = "web/session/authenticate";
   static const String searchRead = "web/dataset/call_kw";
-  static const String callKw = "web/dataset/call_kw";
+  
   static const String getVersionInfo = "web/webclient/version_info";
   static const String getDb9 = "jsonrpc";
   static const String getDb10 = "web/database/list";
   static const String getDb = "web/database/get_list";
   static const String report = "xmlrpc/2/report";
-
+static const String callKw = "web/dataset/call_kw";
   static String getCallKWEndPoint(String model, String method) {
     return callKw; // Use the same endpoint for all call_kw requests
   }
